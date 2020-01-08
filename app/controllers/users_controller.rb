2cc7cfca68a53a2a3c4c822679624e765b7de3cc
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-01-08 17:32:58 -0500

    signin and sessions controllers/user show views set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+      render :show
   end
 
   # GET /users/new
@@ -25,13 +26,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save 
+    session[:user_id] = @user.id
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { render :sessions, location: @new}
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
