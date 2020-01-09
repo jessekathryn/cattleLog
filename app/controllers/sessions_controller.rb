692f87b1e8288fb51ce2beb7e41252afca642e63
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-01-09 15:01:13 -0500

    fixed bcrypt and password user signin and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    binding.pry
     user = User.find_by(username: params[:user][:username])
-    user = user.try(:authenticate, params[:user][:password_confirmation])
+    user = user.try(:authenticate, params[:user][:password])
       return redirect_to(controller: 'sessions', action: 'new') unless user
     session[:user_id] = user.id
     @user = user
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
-    #before_action :require_logged_in
+    before_action :require_logged_in
     
     def home
     end
