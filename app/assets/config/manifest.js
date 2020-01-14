1f892a3ced6933e1ed6141460c9eaa8a7d48ca26
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-01-13 21:20:32 -0500

    home page close to done

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
   //= link_tree ../images
   //= link_directory ../javascripts .js
   //= link_directory ../stylesheets .css
+
+  //= link_tree ./../images .gif
+  //= link_tree ./../images .jpg
+  //= link_tree ./../images .jpeg
+  //= link_tree ./../images .png
