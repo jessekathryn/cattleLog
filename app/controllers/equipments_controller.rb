7d99e8e2ce4e1373258f1b97a6673117d8ec6337
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-01-16 20:44:05 -0500

    reports model

---
+++ app/controllers/equipments_controller.rb
@@ -0,0 +1,55 @@
+class equipmentsController < ApplicationController
+    before_action :set_equipment, only: [:show, :edit, :update, :destroy]
+    before_action :current_user
+  
+  
+    def index
+      @equipments = Equipment.all
+    end
+  
+  
+    def show
+    end
+  
+    def new
+      @equipment = Equipment.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @equipment = Equipment.new(equipment_params)
+  
+        if @equipment.save
+          redirect_to @equipment
+        else
+          render :new
+      end
+    end
+  
+    def update
+        if @equipment.update(equipment_params)
+          redirect_to @equipment
+        else
+          render :edit 
+        end
+    end
+  
+  
+    def destroy
+      @equipment.destroy
+        redirect_to equipments_url
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_equipment
+        @equipment = Equipment.find(params[:id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def equipment_params
+        params.require(:equipment).permit(:equipment_name, :acreage, :crop, :coordinates, :user_id)
+      end
+  end
