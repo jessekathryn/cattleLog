153b16b9938a712291e5b307cfc9bc905903ca33
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-01-28 17:22:32 -0500

    need to fix chart areas and include search bar, add expenses to reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
     has_many :reports
 
     validates :email, uniqueness: true
-    validates :username, uniqueness: true
+    #validates :username, uniqueness: true
     validates :username, length: { minimum: 2, message: "Must be 2 characters long." }
     validates :password, length: { in: 6..20, message: "Must be 6-20 characters long."  }
 
     def self.find_or_create_by_omniauth(auth_hash)
-        self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+        self.where(:email => auth_hash["info"]["email"], :username => auth_hash["info"]["nickname"]).first_or_create do |user|
             user.password = SecureRandom.hex
         end
     end
