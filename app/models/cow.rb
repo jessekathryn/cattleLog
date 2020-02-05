70b2dd9d03dcb42ae4b6cf69d68ce4dd78588527
Jesse Barnes <jessekathrynbarnes@gmail.com>
2020-02-05 03:22:52 -0500

    user_id=(id) defined in cow

--- app/models/cow.rb
+++ app/models/cow.rb
@@ -1,3 +1,12 @@
 class Cow < ApplicationRecord
    belongs_to :user
+
+   def user_id
+      self.try(:user).try(:id)
+    end
+  
+    def user_id=(id)
+      user = user.find_or_create_by(user_id: id)
+      self.user = user
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,6 @@ class User < ApplicationRecord
 
     accepts_nested_attributes_for :cows
 
-
-    def cow_name
-        self.try(:cow).try(:name)
-    end
-
-    def cow_name=(name)
-        cow = Cow.find_or_create_by(name: name)
-        self.cow = cow
-    end
-
     validates :email, uniqueness: true
     validates :username, length: { minimum: 2, message: "Must be 2 characters long." }
     validates :password, length: { in: 6..20, message: "Must be 6-20 characters long." }
